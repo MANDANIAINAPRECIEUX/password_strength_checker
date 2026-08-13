51e2d30049cd3aefd66b17b117ecdb1c61864000
unknown <mandaniainaprecieux@yahoo.com>
2026-08-13 21:33:18 +0300

    initialisation de test spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,23 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+
+
+# frozen_string_literal: true
+
+RSpec.describe PasswordStrengthChecker::Checker do
+  describe "#minimum_length?" do
+    it "retourne true pour un mot de passe d'au moins 8 caractères" do
+      checker = described_class.new("Ruby2026")
+
+      expect(checker.minimum_length?).to be(true)
+    end
+
+    it "retourne false pour un mot de passe de moins de 8 caractères" do
+      checker = described_class.new("Ruby")
+
+      expect(checker.minimum_length?).to be(false)
+    end
+  end
+end
